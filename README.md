102e901f33614c535bac38837d37cb99452fff2d
Moisés Silva <52184685+moisesepsilva2@users.noreply.github.com>
2021-05-23 12:59:27 -0300

    add allure report and other changes

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 clone the repository <br /> 
 run npm install to install project dependencies <br />
 run npm test to execute the test suite <br />
+then run npx allure serve to open the test report <br />
+
+PS: To run the tests it's necessary to have an Android device attached with the package br.com.sky.selfcare previous installed
--- wdio.conf.js
+++ wdio.conf.js
@@ -53,7 +53,6 @@ exports.config = {
         // 5 instances get started at a time.
         //
         platformName:'Android',
-        //'appium:udid':'R9XN606TPDJ',
         'appium:appPackage':'br.com.sky.selfcare',
         'appium:appActivity':'br.com.sky.selfcare.features.main.launch.LaunchActivity',
         // If outputDir is provided WebdriverIO can capture driver session logs
@@ -130,7 +129,11 @@ exports.config = {
     // Test reporter for stdout.
     // The only one supported by default is 'dot'
     // see also: https://webdriver.io/docs/dot-reporter.html
-    reporters: ['spec'],
+    reporters: [['allure', {
+        outputDir: 'allure-results',
+        disableWebdriverStepsReporting: true,
+        disableWebdriverScreenshotsReporting: true,
+    }]],
 
 
     
@@ -266,8 +269,27 @@ exports.config = {
      * @param {Array.<Object>} capabilities list of capabilities details
      * @param {<Object>} results object containing test results
      */
-    // onComplete: function(exitCode, config, capabilities, results) {
-    // },
+     onComplete: function() {
+        const allure = require('allure-commandline');
+        const reportError = new Error('Could not generate Allure report')
+        const generation = allure(['generate', 'allure-results', '--clean'])
+        return new Promise((resolve, reject) => {
+            const generationTimeout = setTimeout(
+                () => reject(reportError),
+                5000)
+
+            generation.on('exit', function(exitCode) {
+                clearTimeout(generationTimeout)
+
+                if (exitCode !== 0) {
+                    return reject(reportError)
+                }
+
+                console.log('Allure report successfully generated')
+                resolve()
+            })
+        })
+    },
     /**
     * Gets executed when a refresh happens.
     * @param {String} oldSessionId session ID of the old session
